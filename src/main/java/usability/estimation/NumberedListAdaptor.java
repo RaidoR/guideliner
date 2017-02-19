769b25af22627af35c89dafc1c9d6c93c159d552
Jevgeni Marenkov <jevgeni.marenkov@gmail.com>
2017-02-19 19:55:53 +0200

    Bugfixing

--- src/main/java/usability/estimation/NumberedListAdaptor.java
+++ src/main/java/usability/estimation/NumberedListAdaptor.java
@@ -34,7 +34,7 @@ public class NumberedListAdaptor extends AbstractAdaptor {
 			List<WebElement> lis = el.findElements(By.tagName("li"));
 			if (lis.size() <= element.getContentLength()) {
 				File file = screenshoter.takeScreenshot(screenshot, el, driver);
-				 result.getFailedElements().add(prepareFailedElement("List", "List", "List contains " + lis.size() + " elements" , file.getName()));
+				 result.getFailedElements().add(prepareFailedElement("List", "List", "List contains " + lis.size() + " elements" , file));
 				 result.setElementType(ElementType.NUMBERED_LIST);
 			}
 		}
